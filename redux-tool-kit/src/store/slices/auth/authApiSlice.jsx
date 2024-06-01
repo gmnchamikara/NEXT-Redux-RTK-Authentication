e0061d7471cceaeea3331511355b39a7e924ee0e
Nuwan Chamikara <39343048+gmnchamikara@users.noreply.github.com>
2024-06-01 11:18:25 +0530

    errors fix

--- redux-tool-kit/src/store/slices/auth/authApiSlice.jsx
+++ redux-tool-kit/src/store/slices/auth/authApiSlice.jsx
@@ -15,3 +15,24 @@
 // export const {
 //     useLoginMutation
 // } = authApiSlice
+
+"use client"
+
+import { fetchBaseQuery } from "@reduxjs/toolkit/dist/query";
+import { apiSlice } from "../../api";
+
+
+const baseEndPoint = process.env.NEXT_PUBLIC_NODE_ENV === "production" ? process.env.NEXT_PUBLIC_BACKEND_PROD : process.env.NEXT_PUBLIC_BACKEND_DEV;
+
+export const authApiSlice = apiSlice.injectEndpoints({
+    baseEndPoint: fetchBaseQuery({ baseUrl: baseEndPoint }),
+    endpoints: builder => ({
+        login: builder.mutation({
+            query: credentials => ({
+                url: '/user/login',
+                method: 'POST',
+                body: { ...credentials }
+            })
+        })
+    })
+})
